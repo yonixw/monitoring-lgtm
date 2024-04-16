7119d8ff3e1a4f2e77a97ae257b9233172c30313
Yoni Water Man <yonixw@gmail.com>
2024-04-16 16:06:15 +0000

    better labels as scopes and multi value. Also timer for gauge

--- pkgs/1-ts-metric-export/src/metrics/counter.ts
+++ pkgs/1-ts-metric-export/src/metrics/counter.ts
@@ -3,43 +3,29 @@ import Prometheus from "prom-client";
 
 const counter = new Prometheus.Counter({
   name: "counter_example",
-  help: "Counter is integer from 0 supports: add(num=1), reset()",
-  labelNames: ["labelC1"] as const,
+  help: "Counter is an increasing float from 0 supports: add(num=1), reset()",
+  labelNames: ["scope_Counter", "multiscope_Counter"] as const,
   aggregator: "sum" // 'sum', 'first', 'min', 'max', 'average' or 'omit'
 });
 
-// inc() is updating a single label
-//      starts from 0
-// reset() is reseting ALL labels,
-//      making them disapper from /metrics until next "inc"
-
 export async function setupCounter(app: Express) {
-  app.get(["/counter/add", "/counter/add/:num"], async (req, res) => {
-    const add = parseInt(req.params.num) || 0;
-    if (add !== 0) {
-      counter.inc(add); // NULL label only
-      counter.inc({ labelC1: add });
-    } else {
-      counter.inc(); // NULL label only
-      counter.inc({ labelC1: 1 });
-    }
-    res.send({ counter: await counter.get() });
-  });
 
   app.get(["/counter/addf", "/counter/addf/:num"], async (req, res) => {
     const add = parseFloat(req.params.num) || 0;
     if (add !== 0) {
       counter.inc(add); // NULL label only
-      counter.inc({ labelC1: add });
+      counter.inc({ scope_Counter: "example1" }, add);
+      counter.inc({ scope_Counter: "example1", multiscope_Counter: "host:port" }, add);
     } else {
       counter.inc(); // NULL label only
-      counter.inc({ labelC1: 1 });
+      counter.inc({ scope_Counter: "example1" });
+      counter.inc({ scope_Counter: "example1", multiscope_Counter: "host:port" });
     }
     res.send({ counter: await counter.get() });
   });
 
   app.get(["/counter/reset"], async (req, res) => {
-    counter.reset(); // Reset ALL labels
+    counter.reset(); // Reset ALL label scopes
     res.send({ counter: await counter.get() });
   });
 
--- pkgs/1-ts-metric-export/src/metrics/gauge.ts
+++ pkgs/1-ts-metric-export/src/metrics/gauge.ts
@@ -3,27 +3,32 @@ import Prometheus from "prom-client";
 
 const gauge = new Prometheus.Gauge({
     name: "gauge_example",
-    help: "Gauge is a number with: inc(),dec(),set(),reset()",
-    labelNames: ["labelG1", "labelTimer1", "labelTimer2"] as const,
+    help: "Gauge is almost like float with: inc(n=1),dec(n=1),set(),reset(); example show set to time()",
+    labelNames: ["gauge_Scope", "multiscope_Gauge"] as const,
     aggregator: "sum" // 'sum', 'first', 'min', 'max', 'average' or 'omit' 
 });
 
-const reportTiming = gauge.startTimer({ labelTimer1: 1, labelTimer2: 2 });
+const uptimeTimer = gauge.startTimer({ gauge_Scope: "timer" });
 
 export async function setupGauge(app: Express) {
     app.get("/gauge/timer", async (req, res) => {
-        const tickTime = reportTiming();
-        res.send({ tickTime, gauge: await gauge.get() })
+        const requestTimer = gauge.startTimer({ gauge_Scope: "req_timer" });
+        res.send({
+            uptimeSecs: uptimeTimer(), requestSecs: requestTimer(),
+            gauge: await gauge.get()
+        })
     })
 
     app.get(["/gauge/addf", "/gauge/addf/:num"], async (req, res) => {
         const add = parseFloat(req.params.num) || 0;
         if (add !== 0) {
             gauge.inc(add); // NULL label only
-            gauge.inc({ labelG1: add });
+            gauge.inc({ gauge_Scope: "example1" }, add);
+            gauge.inc({ gauge_Scope: "example1", multiscope_Gauge: "host:port" }, add);
         } else {
             gauge.inc(); // NULL label only
-            gauge.inc({ labelG1: 1 });
+            gauge.inc({ gauge_Scope: "example1" });
+            gauge.inc({ gauge_Scope: "example1", multiscope_Gauge: "host:port" });
         }
         res.send({ gauge: await gauge.get() });
     });
@@ -32,10 +37,12 @@ export async function setupGauge(app: Express) {
         const dec = parseFloat(req.params.num) || 0;
         if (dec !== 0) {
             gauge.dec(dec); // NULL label only
-            gauge.dec({ labelG1: dec });
+            gauge.dec({ gauge_Scope: "example1" }, dec);
+            gauge.dec({ gauge_Scope: "example1", multiscope_Gauge: "host:port" }, dec);
         } else {
             gauge.dec(); // NULL label only
-            gauge.dec({ labelG1: 1 });
+            gauge.dec({ gauge_Scope: "example1" });
+            gauge.dec({ gauge_Scope: "example1", multiscope_Gauge: "host:port" });
         }
         res.send({ gauge: await gauge.get() });
     });
@@ -44,10 +51,12 @@ export async function setupGauge(app: Express) {
         const _set = parseFloat(req.params.num) || 0;
         if (_set !== 0) {
             gauge.set(_set); // NULL label only
-            gauge.set({ labelG1: 1 }, _set);
+            gauge.set({ gauge_Scope: "example1" }, _set);
+            gauge.set({ gauge_Scope: "example1", multiscope_Gauge: "host:port" }, _set);
         } else {
             gauge.set(0); // NULL label only
-            gauge.set({ labelG1: 1 }, 1);
+            gauge.set({ gauge_Scope: "example1" }, 0);
+            gauge.set({ gauge_Scope: "example1", multiscope_Gauge: "host:port" }, 0);
         }
         res.send({ gauge: await gauge.get() });
     });
